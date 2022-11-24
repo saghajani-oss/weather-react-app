69e2804d7c58cc4567d34383936a2cc0c316a1e6
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2022-11-24 11:33:42 +0330

    making some changes in footer

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Samar Weather App</h1>
-      <p>
+      <footer>
         This app is coded by{" "}
-        <a href="http://samiraaghajani.ir/" target="_blank">Samira Aghajani</a> and it is{" "}
-        <a href=''>open-source</a>
-      </p>
+        <a href="http://samiraaghajani.ir/" target="_blank">
+          Samira Aghajani
+        </a>{" "}
+        and it is{" "}
+        <a href="https://github.com/saghajani-oss/weather-react-app" target="_blank">
+          open-source
+        </a>
+      </footer>
     </div>
   );
 }
